2615df8c593ad4cbb3ba84ee9d6dde19bc4052c4
Aaron Ambrosi <aaambros@c2s11d51.42wolfsburg.de>
2024-01-05 14:49:42 +0100

    putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: aaambros <aaambros@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 23:28:32 by aaambros          #+#    #+#             */
-/*   Updated: 2023/12/15 16:03:53 by aaambros         ###   ########.fr       */
+/*   Updated: 2024/01/05 14:49:32 by aaambros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,3 +68,44 @@ int	ft_putnbr(int n)
 	free(num);
 	return (len);
 }
+
+// #include "ft_printf.h"
+
+// int	ft_calclen(int n)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	if (n == 0)
+// 		i++;
+// 	while (n != 0)
+// 	{
+// 		n = n / 10;
+// 		i++;
+// 	}
+// 	return (i);
+// }
+
+// int	ft_putnbr_pf(int n)
+// {
+// 	int	count;
+
+// 	count = ft_calclen(n);
+// 	if (n == -2147483648)
+// 		return (ft_putstr_pf("-2147483648"));
+// 	else
+// 	{
+// 		if (n < 0)
+// 		{
+// 			ft_putchar_pf('-');
+// 			n = -n;
+// 			count++;
+// 		}
+// 		if (n > 9)
+// 		{
+// 			ft_putnbr_pf(n / 10);
+// 		}
+// 		ft_putchar_pf((n % 10) + '0');
+// 	}
+// 	return (count);
+// }
